c052ffd2d42f979d4669fbf524c35e7dbb08fae6
Hamzah Jomaa <hamzah.m.jomaa@gmail.com>
2022-04-03 21:31:54 +0300

    connection

---
+++ apis/connection.php
@@ -0,0 +1,16 @@
+<?php
+
+$server = "db-mysql-nyc3-05704-do-user-10544311-0.b.db.ondigitalocean.com";
+$username = "doadmin";
+$password = "wpitp2pT8Ji2wjJo";
+$db = "turbodb";
+$port = 25060;
+
+$connection = new mysqli($server,$username,$password,$db,$port);
+
+// Check connection
+if ($conn->connect_error) {
+    die("Connection failed: " . $conn->connect_error);
+}
+
+?>
